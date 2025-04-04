a1c0b964fffb7b63f8ec8b1dd542c85229122745
puertasman <puertasman@gmail.com>
2025-04-04 14:23:34 +0200

    al día

---
+++ js/script.js
@@ -0,0 +1,15 @@
+import { personajesAnimados } from './personajes.js'
+
+let table = document.querySelector("#tablaPersonajes")
+
+if (personajesAnimados.length > 0){
+    // hay cosas
+}
+else{
+    // no hay cosas
+    let p = document.createElement("p")
+
+    p.textContent = `La lista está vacía.`
+
+    table.appendChild(p)
+}
